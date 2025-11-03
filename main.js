a1b8936fdd9ba0c93ccc74e0c1539457e78e72fd
praveen <praveen@praveens-MacBook-Air.local>
2025-11-03 18:38:38 +0530

    Updated the projects section with swiper

--- main.js
+++ main.js
@@ -6,3 +6,17 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+const swiper = new Swiper('.project-swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+});
